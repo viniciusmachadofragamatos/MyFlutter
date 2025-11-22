88c36f0333a255cb46020358c8105be8539a1a3f
ViniciusMachado <viniciusmachadofragamatos8@gmail.com>
2025-11-22 12:16:18 -0300

    Update login_page.dart
    Estilização do login, concertando o erro de o teclado sobrepor componentes com SingleChildScrollView, utilização do TextField, Botão visibility e visibility.off, utilização de decoraators e InkWell para botões e trocando de page para home ao entrar

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,85 +1,184 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:meu_app/pages/main_page.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
   State<StatefulWidget> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController(text: "");
+  TextEditingController senhaController = TextEditingController(text: ""); 
+  bool isObscureText = true;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 223, 219, 219),
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 100
+        backgroundColor: const Color.fromARGB(255, 242, 238, 235),
+          body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 100),
+                Row(
+                  children: [
+                    Expanded(child: Container()),
+                    Expanded(
+                      flex: 5,
+                      child: Image.asset('assets/images/logo.jpeg'),
+                    ),
+                    Expanded(child: Container())
+                  ],
                 ),
-                const Icon(Icons.person, size: 150,color: Color.fromARGB(255, 13, 153, 196),),
-                SizedBox(
-                height: 50
+                SizedBox(height: 20),
+                const Text(
+                  "Já tem cadastro?",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
                 ),
+                SizedBox(height: 10),
+                const Text("Faça seu login e make the change_",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(height: 40),
                 Container(
                   width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
-                  //color: Colors.green,
+                  margin: EdgeInsets.symmetric(horizontal: 30),
                   height: 30,
                   alignment: Alignment.center,
-                  child: Row(
-                    
-                    children: [
-                      Expanded(flex: 2, child: Text("Informe seu email:")),
-                      SizedBox(width: 20),
-                      Expanded(flex: 3,child: Text("Email")),
-                    ],
+                  child: TextField(
+                    controller: emailController,
+                    onChanged: (value){
+                      debugPrint(value);
+                    },
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(top: 0),
+                      enabledBorder:  UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.cyan)
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.cyan
+                        )
+                      ),
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Colors.cyan),
+                      prefixIcon: Icon(Icons.person, color: Colors.cyan,),
+                    ),
+                  )
                   ),
-                ),
-                SizedBox(
-                height: 10
-                ),
+                SizedBox(height: 20),
                 Container(
                   width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
-                  //color: Colors.green,
+                  margin: EdgeInsets.symmetric(horizontal: 30),
                   height: 30,
                   alignment: Alignment.center,
-                  child: Row(
-                    children: [
-                      Expanded(flex:2,child: Text("Informe Sua Senha:")),
-                      SizedBox(width: 20,),
-                      Expanded(flex:3,child: Text("Senha")),
-                    ],
-                  ),
+                  child: TextField(
+                    controller: senhaController,
+                    obscureText: isObscureText,
+                    onChanged: (value){
+                      debugPrint(emailController.text);
+                      debugPrint(senhaController.text);
+                    },
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(top: 0),
+                      enabledBorder:  UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.cyan)
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.cyan
+                        )
+                      ),
+                      hintText: "Senha",
+                      hintStyle: TextStyle(color: Colors.cyan),
+                      prefixIcon: Icon(Icons.lock, color: Colors.cyan),
+                      suffixIcon:
+                          InkWell(
+                            onTap: (){
+                              setState(() {   
+                              isObscureText = !isObscureText;
+                            });
+                          },
+                        child : Icon(
+                          isObscureText ? Icons.visibility_off : Icons.visibility,
+                           color: Colors.cyan,
+                           )
+                           ),
+                    ),
+                  )
+                
+                ),
+                SizedBox(height: 30),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                      width: double.infinity,
+                      child: TextButton(
+                          onPressed: () {
+                            if(emailController.text.trim() == "email@email.com" && senhaController.text.trim() == "123"){
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainPage()));
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("Erro ao efetuar o Login"))
+                              );
+                              //debugPrint("Erro ao efetuar o Login");
+                            }
+                            //debugPrint(emailController.text);
+                            //debugPrint(senhaController.text);
+                          },
+                          style: ButtonStyle(
+                              shape: WidgetStatePropertyAll(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)
+                                )
+                              ),
+                              backgroundColor: WidgetStatePropertyAll(
+                                  const Color.fromARGB(255, 24, 165, 207))),
+                          child: Text(
+                            "ENTRAR",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          ))),
                 ),
                 Expanded(child: Container()),
                 Container(
                   width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
-                  color: Colors.green,
+                  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                   height: 30,
                   alignment: Alignment.center,
-                  child: Text("Login"),
+                  child: Text(
+                    "Esqueci minha senha",
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 24, 165, 207),
+                        fontWeight: FontWeight.w400),
+                  ),
                 ),
                 Container(
                   width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                   height: 30,
                   alignment: Alignment.center,
-                  child: Text("Cadastrar"),
-                ),SizedBox(
-                height: 20
+                  child: Text(
+                    "Criar Conta",
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 24, 165, 207),
+                        fontWeight: FontWeight.w400),
+                  ),
                 ),
-
-            ],
+                SizedBox(height: 60),
+              ],
+            ),
           ),
-        ),
+          ),
       ),
     );
   }
